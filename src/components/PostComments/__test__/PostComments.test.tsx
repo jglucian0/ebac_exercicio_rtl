5d0e8bdca9f970cf78b34f6aa7ab09861d0039df
jglucian0 <jgluciano.luz1@gmail.com>
2024-11-06 22:26:44 -0300

    Update PostComments.test.tsx

--- src/components/PostComments/__test__/PostComments.test.tsx
+++ src/components/PostComments/__test__/PostComments.test.tsx
@@ -1,7 +1,6 @@
-import PostComments from "..";
-
 import "@testing-library/jest-dom/extend-expect";
 import { fireEvent, render, screen } from "@testing-library/react";
+import PostComments from "..";
 
 describe("Deve testar funcionalidade do PostComments", () => {
   test("Deve criar e enviar dois comentários", () => {
@@ -12,19 +11,17 @@ describe("Deve testar funcionalidade do PostComments", () => {
     });
     expect(commentTextarea).toHaveValue("Primeiro comentário");
     const submitButton = screen.getByRole("button", { name: /comentar/i });
-
-
+    
     fireEvent.click(submitButton);
-
-
+    
     expect(screen.getByText("Primeiro comentário")).toBeInTheDocument();
     fireEvent.change(commentTextarea, {
       target: { value: "Segundo comentário" },
     });
     expect(commentTextarea).toHaveValue("Segundo comentário");
-
+    
     fireEvent.click(submitButton);
-
+    
     expect(screen.getByText("Segundo comentário")).toBeInTheDocument();
     expect(screen.getAllByTestId("post-comment")).toHaveLength(2);
   });
